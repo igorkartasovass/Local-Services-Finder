fa4fa79ceb482e479bdb22167e071851a79be024
igorkartasovass <igor.kartasovas7@gmail.com>
2025-05-27 19:20:18 -0700

    Added service controller with basic GET and POST handlers

---
+++ server/controllers/serviceController.js
@@ -0,0 +1,23 @@
+const Service = require('../models/Service');
+
+exports.getServices = async(req, res) => {
+    try{
+        const services = await Service.find();
+        res.json(services);
+    }catch(err) {
+        res.status(500).json({message: err.mesaage});
+    }
+};
+
+exports.createService = async (req, res) => {
+    const { name, type, address, coordinates } = req.body ;
+
+    const service = new Service({ name, type, address, coodrinates});
+
+    try{
+        const savedService = await service.save();
+        res.status(201).json(savedService);
+    }catch(err){
+        res.status(400).json({message: err.message});
+    }
+}
